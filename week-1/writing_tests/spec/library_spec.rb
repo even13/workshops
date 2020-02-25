4db1b8587c9ef1bc1390df55eafdaa21797c9efe
EC2 Default User <ec2-user@ip-172-31-42-246.eu-west-1.compute.internal>
2020-02-25 16:01:34 +0000

    refactor: before each block in test suite

--- week-1/writing_tests/spec/library_spec.rb
+++ week-1/writing_tests/spec/library_spec.rb
@@ -1,26 +1,27 @@
 require 'library'
 
 describe Library do
+  before(:each) do
+    @library = Library.new
+  end
+    
   it 'can find a specific book' do
-    library = Library.new
-    book = library.find_book('Eloquent JavaScript')
+    book = @library.find_book('Eloquent JavaScript')
     expect(book[:title]).to eq('Eloquent JavaScript')
     expect(book[:author]).to eq('Marijn Haverbeke')
     expect(book[:subject]).to eq('JS')
   end
 
   it 'can add a new book' do
-    library = Library.new
     new_book = {title: 'Ruby on Rails', author: 'unknown', subject: 'Rails'}
-    library.add_book(new_book)
-    expect(library.books).to include(new_book)
+    @library.add_book(new_book)
+    expect(@library.books).to include(new_book)
   end
 
   it 'can remove a book' do
-    library = Library.new
     removed_book = {title: 'POODR', author: 'Sandi Metz', subject: 'OOP'}
-    library.remove_book('POODR')
-    expect(library.books).not_to include(removed_book)
+    @library.remove_book('POODR')
+    expect(@library.books).not_to include(removed_book)
   end
 
   it 'can list all the books on a specific subject' do
