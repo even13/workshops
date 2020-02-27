712d30dde48a9487c481d0d4418656366bc0f824
Eve Noirault <eve.noirault@live.fr>
2020-02-27 16:45:53 +0000

    test: double feedback in student class, #happiness

--- week-1/mocking_1/spec/student_spec.rb
+++ week-1/mocking_1/spec/student_spec.rb
@@ -4,6 +4,8 @@ require 'feedback'
 
 describe Student do
   let(:date) { "2020-02-27" }
+  let(:feedback_1) { double :feedback  }
+  let(:feedback_2) { double :feedback }
   subject(:student) { Student.new }
 
   describe '#full_name' do
@@ -20,8 +22,10 @@ describe Student do
 
   describe '#happiness' do
     it 'returns the average happiness reported' do
-      feedback_1 = Feedback.new(4, date)
-      feedback_2 = Feedback.new(4, date)
+      # feedback_1 = Feedback.new(4, date)
+      # feedback_2 = Feedback.new(4, date)
+      allow(feedback_1).to receive(:happiness) {4}
+      allow(feedback_2).to receive(:happiness) {4}
       student = Student.new(feedbacks: [feedback_1, feedback_2])
 
       expect(student.happiness).to eq 4
