81bfc31f667d01fb69c1dc9209df5162622bae0f
Eve Noirault <eve.noirault@live.fr>
2020-03-10 13:41:42 +0000

    callbacks exercise 1

--- week-5/callbacks_following_the_flow_of_control/index.js
+++ week-5/callbacks_following_the_flow_of_control/index.js
@@ -3,4 +3,13 @@ console.log('hello!')
 $.get("https://async-workshops-api.herokuapp.com/people", function(peopleResponse) {
    $("#person").html(peopleResponse[0].name);
 });
+console.log('Question 1')
+console.log(1)
 
+$(document).click(function(clickEvent) {
+  console.log(3)
+  console.log("The click event:", clickEvent);
+  console.log(4)
+});
+
+console.log(2)
